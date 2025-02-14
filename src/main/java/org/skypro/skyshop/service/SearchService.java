4781bba81fa5d2fad01db221d27d2deae1836f23
1в <cykalov.aleksand@mail.ru>
2025-02-14 22:35:41 +0300

    Проверьте пожалуйста домашнее задание, указанные недостатки как понял устранил

--- src/main/java/org/skypro/skyshop/service/SearchService.java
+++ src/main/java/org/skypro/skyshop/service/SearchService.java
@@ -16,13 +16,13 @@ public class SearchService {
     }
 
     public List<SearchResult> search(String query) {
-        Map<UUID, Searchable> collect = searchService.values().stream().filter(Objects::nonNull)
+       Map<UUID, Searchable> collect = searchService.values().stream().filter(Objects::nonNull)
                 .filter(product -> product.sortingElement().equalsIgnoreCase(query.trim()))
                 .collect(Collectors.toMap(Searchable::getId, product -> product));
         ArrayList<SearchResult> variant;
         variant = (ArrayList<SearchResult>) collect.values().stream()
                 .map(SearchResult::fromSearchable).collect(Collectors.toList());
-        return variant;
+               return variant;
     }
 
 }
