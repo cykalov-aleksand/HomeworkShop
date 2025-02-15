ba9801dbed67647aba8c62c692f8067bab91104d
1в <cykalov.aleksand@mail.ru>
2025-02-15 10:02:13 +0300

    Проверьте пожалуйста домашнее задание, указанные недостатки как понял устранил

--- src/test/java/org/skypro/skyshop/SearchServiceTest.java
+++ src/test/java/org/skypro/skyshop/SearchServiceTest.java
@@ -1,6 +1,8 @@
 package org.skypro.skyshop;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.skypro.skyshop.model.article.Article;
 import org.skypro.skyshop.model.product.Product;
 import org.skypro.skyshop.model.product.SimpleProduct;
@@ -10,29 +12,45 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 import static java.util.Arrays.stream;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-
+@ExtendWith(MockitoExtension.class)
 public class SearchServiceTest {
 
     @Test
     void searchIfThereAreNoObjectsIn_StorageServis_AnEmpty() {
+        String stringStates;
         System.out.println("Поиск в случае отсутствия объектов в StorageService:\n");
         CodeAnalysisSearchServices test = new CodeAnalysisSearchServices("Макароны", null);
-        System.out.println(test.test());
+        stringStates=test.test();
+        System.out.println(stringStates);
+       // boolean equals = stringStates.equals("Тестовые значения в StorageService не введены");
+        assertEquals("Тестовые значения в StorageService не введены",stringStates);
+
     }
 
     @Test
     void searchIfThereAreObjectsButThereIsNoSuitableOneIn_StorageService() {
+        String stringStates;
         System.out.println("Поиск в случае, если объекты в StorageService есть, но нет подходящего:\n");
         CodeAnalysisSearchServices test = new CodeAnalysisSearchServices("Мыло", test());
-         System.out.println(test.test());
+        stringStates=test.test();
+        System.out.println(stringStates);
+        assertEquals("[]",stringStates);
     }
 
     @Test
     void searchWhenThereIsSuitableObjectIn_StorageService() {
+        String stringStates;
+        String stringCheck="Пельмени";
         System.out.println("Поиск, когда есть подходящий объект в StorageService:\n");
-        CodeAnalysisSearchServices test = new CodeAnalysisSearchServices("Пельмени", test());
-        System.out.println(test.test());
+        CodeAnalysisSearchServices test = new CodeAnalysisSearchServices(stringCheck, test());
+        stringStates=test.test();
+        System.out.println( stringStates);
+        assertTrue(stringStates.contains(stringCheck));
+
+
     }
 
     private Map<UUID, Searchable> test() {
@@ -51,4 +69,4 @@ public class SearchServiceTest {
         testArray.putAll(stream(products).collect(Collectors.toMap(Searchable::getId, product -> product)));
         return testArray;
     }
-}
+  }
